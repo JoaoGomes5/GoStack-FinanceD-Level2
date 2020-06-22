047e59878cf1b948e1a6b3cf6677ee25b8a76e58
JoJo <joaopfg.2002@gmail.com>
2020-06-22 23:12:52 +0100

    Final tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,18 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome'
 }
 
 class TransactionsRepository {
@@ -14,15 +23,47 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const inicialValue = 0;
+
+    const income = this.transactions.reduce(
+      (acumulador, valorAtual) =>
+        valorAtual.type === 'income'
+          ? acumulador + valorAtual.value
+          : acumulador + 0,
+      inicialValue,
+    );
+
+    const outcome = this.transactions.reduce(
+      (acumulador, valorAtual) =>
+        valorAtual.type === 'outcome'
+          ? acumulador + valorAtual.value
+          : acumulador + 0,
+      inicialValue,
+    );
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO) : Transaction {
+
+      const transaction = new Transaction({title, value, type});
+
+      this.transactions.push(transaction);
+
+      return transaction;
   }
 }
 
